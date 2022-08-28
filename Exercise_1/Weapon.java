220e069cca8fd0c432e6f93a131024cf41193654
wronnwronn <111344165+wronnwronn@users.noreply.github.com>
2022-08-28 20:21:39 +0800

    Create Weapon.java

---
+++ Exercise_1/Weapon.java
@@ -0,0 +1,17 @@
+public class Weapon {
+    // Attributes
+    String name;
+    String object;
+    int damage;
+    double Weight;
+    String Rarity;
+    String Description;
+  
+    public void Weaponlore() {
+      System.out.println("MyWeapon: " + name);
+      System.out.println("MyDamage:" + damage);
+      System.out.println("MyWeight: " + Weight);
+      System.out.println("MyRarity: " + Rarity);
+      System.out.println("MyDescription: " + Description);
+    }
+  }
